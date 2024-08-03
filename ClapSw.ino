82a3910c4ad190440bef652a40bcf1f690276532
ranjini022 <114769902+ranjini022@users.noreply.github.com>
2024-08-03 13:27:35 +0530

    ClapSw.ino

--- ClapSw.ino
+++ ClapSw.ino
@@ -1,9 +1,6 @@
-// Clap Light Switch
-// Serhii Bazavluk | March 2021
-// YouTube: https://www.youtube.com/electricalpro
 
 int mic = 2;       // microphone module
-int indicLED = 13; // indication pin
+int indiLED = 13; // indication pin
 int lightPin = 9;  // relay (lights control)
 bool lightState = true;
 bool ignoreInt = false;
@@ -12,7 +9,7 @@ uint32_t ClapDetetedTime [3] {};
 
 void setup() {
   Serial.begin(115200);
-  pinMode(indicLED, OUTPUT);    
+  pinMode(indiLED, OUTPUT);    
   pinMode(lightPin, OUTPUT);  
   digitalWrite(lightPin, lightState);
   attachInterrupt(digitalPinToInterrupt(mic), ISR_ClapDetected, FALLING);
@@ -23,13 +20,13 @@ delay(2);
 
 if(millis() - ClapDetetedTime[2] >= 800)
 {
-  digitalWrite(indicLED, LOW);
+  digitalWrite(indiLED, LOW);
 }
 
 if(millis() - ClapDetetedTime[2] < 800 && millis() - ClapDetetedTime[1] < 800 && ClapDetected && ClapDetetedTime[1] - ClapDetetedTime[0]>=800)
   {
     Serial.println("double clap detected");
-    digitalWrite(indicLED, LOW);
+    digitalWrite(indiLED, LOW);
     Serial.print("Setting light to ");
     Serial.println(String(!lightState));
     digitalWrite(lightPin, lightState);
@@ -49,6 +46,6 @@ void ISR_ClapDetected()
   ClapDetetedTime[2] = millis();
   ClapDetected = true;
   Serial.println("clap detected");
-  digitalWrite(indicLED, HIGH);
+  digitalWrite(indiLED, HIGH);
   }
 }
